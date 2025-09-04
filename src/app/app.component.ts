8fb1433753c99cbaf1d0976036a16931093fa56e
denys.lysenko <denys.lysenko@idealscorp.com>
2025-09-04 13:16:48 +0300

    Update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  protected title = 'pdp-task-3';
+  protected title = 'pdp-task-32';
 }
