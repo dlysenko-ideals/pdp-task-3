8a3dbce9aacf443569b72f555155ea312361cbb0
denys.lysenko <denys.lysenko@idealscorp.com>
2025-09-04 13:11:38 +0300

    Update caching stratagy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  protected title = 'pdp-task-32';
+  protected title = 'pdp-task-3';
 }
